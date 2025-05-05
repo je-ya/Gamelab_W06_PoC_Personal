3ac2e5ffe230b09b620ee07f5d978d60ab2eb130
je-ya <psychicartil@gmail.com>
2025-05-05 16:14:22 +0900

    NPC 반응 배경 수정

---
+++ Assets/Scripts/ChatBGBox.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+[ExecuteAlways] // 에디터에서도 작동
+public class ChatBGBox : MonoBehaviour
+{
+    public TextMeshProUGUI textComponent;
+    RectTransform imageRectTransform;
+    public Vector2 padding = new Vector2(20, 20); // 텍스트 외부 여백
+
+    void Start()
+    {
+        imageRectTransform = GetComponent<RectTransform>();
+    }
+
+    void Update()
+    {
+        if (textComponent != null && imageRectTransform != null)
+        {
+            // 텍스트의 실제 출력 크기 가져오기
+            float width = textComponent.preferredWidth;
+            float height = textComponent.preferredHeight;
+
+            // 이미지 크기 조정 (텍스트 크기 + 여백)
+            imageRectTransform.sizeDelta = new Vector2(width + padding.x, height + padding.y);
+        }
+    }
+}
+
